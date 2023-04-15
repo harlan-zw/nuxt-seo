285008fe44a94d1e59e916e490dedf3f3ffc803c
Harlan Wilton <harlan@harlanzw.com>
2023-04-15 12:39:13 +0700

    Update i18n.ts

--- layer/mocks/i18n.ts
+++ layer/mocks/i18n.ts
@@ -1,7 +1,7 @@
 import { ref } from '#imports'
 
 export const useI18n = () => {
-  console.warn('[nuxt-seo-kit]: You must have the `@nuxtjs/i18n@` module installed to use i18n features.')
+  console.warn('[nuxt-seo-kit]: You must have the `@nuxtjs/i18n` module installed to use i18n features.')
   return {
     t(key: string) { return key },
     localeProperties: ref({ code: 'en' }),
