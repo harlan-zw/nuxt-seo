3d635dd3e203729283d70163c4205d77e1861be9
Harlan Wilton <harlan@harlanzw.com>
2023-04-15 12:37:52 +0700

    chore: improved warning message

--- layer/mocks/i18n.ts
+++ layer/mocks/i18n.ts
@@ -1,7 +1,7 @@
 import { ref } from '#imports'
 
 export const useI18n = () => {
-  console.warn('[nuxt-seo-kit]: `useI18n` composable was not found, using a mock composable to continue functioning normally. make sure @nuxtjs/i18n module is loaded before nuxt-seo-kit.')
+  console.warn('[nuxt-seo-kit]: You must have the `@nuxtjs/i18n@` module installed to use i18n features.')
   return {
     t(key: string) { return key },
     localeProperties: ref({ code: 'en' }),
