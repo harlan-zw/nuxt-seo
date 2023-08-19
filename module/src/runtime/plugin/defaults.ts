0c6d5d4dab13d064f0b69c8d94da7a62cbcafda6
Harlan Wilton <harlan@harlanzw.com>
2023-08-19 13:47:37 +0200

    chore: maybe fix broken ctx

--- module/src/runtime/plugin/defaults.ts
+++ module/src/runtime/plugin/defaults.ts
@@ -21,53 +21,55 @@ function titleCase(s: string) {
 
 export default defineNuxtPlugin({
   name: 'nuxtseo:defaults',
-  setup() {
+  setup(nuxtApp) {
     // get the head instance
     const siteConfig = useSiteConfig() as SiteConfig
     delete siteConfig._context
 
-    const route = useRoute()
-    const resolveUrl = createSitePathResolver({ withBase: true, absolute: true })
-    const canonicalUrl = computed(() => resolveUrl(route.path || '/').value)
+    nuxtApp.vueApp.runWithContext(() => {
+      const route = useRoute()
+      const resolveUrl = createSitePathResolver({ withBase: true, absolute: true })
+      const canonicalUrl = computed(() => resolveUrl(route.path || '/').value)
 
-    const title = computed(() => {
-      if (typeof route.meta?.title === 'string')
-        return route.meta?.title
+      const title = computed(() => {
+        if (typeof route.meta?.title === 'string')
+          return route.meta?.title
 
-      // if no title has been set then we should use the last segment of the URL path and title case it
-      const path = route.path || '/'
-      const lastSegment = path.split('/').pop()
-      return lastSegment ? titleCase(lastSegment) : null
+        // if no title has been set then we should use the last segment of the URL path and title case it
+        const path = route.path || '/'
+        const lastSegment = path.split('/').pop()
+        return lastSegment ? titleCase(lastSegment) : null
+      })
+
+      useHead({
+        templateParams: { site: () => siteConfig, separator: siteConfig.titleSeparator },
+        // TODO fetch locale using i18n
+        htmlAttrs: { lang: () => siteConfig.locale },
+        titleTemplate: '%s %separator %site.name',
+        title,
+        link: [{ rel: 'canonical', href: canonicalUrl }],
+      })
+
+      useSeoMeta({
+        ogUrl: canonicalUrl,
+        // TODO integrate with nuxt/i18n
+        ogLocale: siteConfig.defaultLocale,
+        ogSiteName: siteConfig.name,
+        description: siteConfig.description,
+        // extra og are set by InferSeoMeta plugin
+      })
+
+      // init vendors
+      defineOgImage()
+      defineRobotMeta()
+      useSchemaOrg([
+        defineWebSite({
+          name: () => siteConfig?.name || '',
+          inLanguage: () => siteConfig?.locale || '',
+          description: () => siteConfig?.description || '',
+        }),
+        defineWebPage(),
+      ])
     })
-
-    useHead({
-      templateParams: { site: () => siteConfig, separator: siteConfig.titleSeparator },
-      // TODO fetch locale using i18n
-      htmlAttrs: { lang: () => siteConfig.locale },
-      titleTemplate: '%s %separator %site.name',
-      title,
-      link: [{ rel: 'canonical', href: canonicalUrl }],
-    })
-
-    useSeoMeta({
-      ogUrl: canonicalUrl,
-      // TODO integrate with nuxt/i18n
-      ogLocale: siteConfig.defaultLocale,
-      ogSiteName: siteConfig.name,
-      description: siteConfig.description,
-      // extra og are set by InferSeoMeta plugin
-    })
-
-    // init vendors
-    defineOgImage()
-    defineRobotMeta()
-    useSchemaOrg([
-      defineWebSite({
-        name: () => siteConfig?.name || '',
-        inLanguage: () => siteConfig?.locale || '',
-        description: () => siteConfig?.description || '',
-      }),
-      defineWebPage(),
-    ])
   },
 })
