931048642db77f0f957ebff6dd61fbce368a197d
Harlan Wilton <harlan@harlanzw.com>
2023-09-10 00:54:39 +0200

    chore: fix test

--- .test/generate.test.ts
+++ .test/generate.test.ts
@@ -1,28 +1,42 @@
-import { readFile } from 'node:fs/promises'
 import { describe, expect, it } from 'vitest'
-import { buildNuxt, createResolver, loadNuxt } from '@nuxt/kit'
+import { createResolver } from '@nuxt/kit'
+import { $fetch, setup } from '@nuxt/test-utils'
+
+const { resolve } = createResolver(import.meta.url)
+
+await setup({
+  rootDir: resolve('../.playground'),
+})
 
 describe('generate', () => {
   it('basic', async () => {
-    process.env.NODE_ENV = 'production'
-    process.env.prerender = true
-    const { resolve } = createResolver(import.meta.url)
-    const rootDir = resolve('../.playground')
-    const nuxt = await loadNuxt({
-      rootDir,
-      overrides: {
-        _generate: true,
-      },
+    const index = await $fetch('/')
+    // list meta tags with their contents
+    const metaTags = index.match(/<meta.*?>/g) || []
+    // we need to convert any urls with port to localhost, i.e "127.0.0.1:43889" becomes "localhost"
+    metaTags.forEach((tag, i) => {
+      metaTags[i] = tag.replace(/127\.0\.0\.1:\d+/g, 'localhost')
     })
-
-    await buildNuxt(nuxt)
-
-    const index = await readFile(resolve(rootDir, '.output/public/index.html'), 'utf-8')
-
-    expect(index).toContain('link rel="canonical" href="https://harlanzw.com"')
-    expect(index).toContain('<meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">')
-    expect(index).toContain('<script type="application/ld+json" id="schema-org-graph"')
-
-    // @todo snapshot testing
+    expect(metaTags).toMatchInlineSnapshot(`
+      [
+        "<meta charset=\\"utf-8\\">",
+        "<meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\">",
+        "<meta property=\\"og:type\\" content=\\"website\\">",
+        "<meta property=\\"og:image\\" content=\\"http://localhost/__og_image__/og.png\\">",
+        "<meta property=\\"og:image:width\\" content=\\"1200\\">",
+        "<meta property=\\"og:image:height\\" content=\\"630\\">",
+        "<meta property=\\"og:image:type\\" content=\\"image/png\\">",
+        "<meta name=\\"twitter:card\\" content=\\"summary_large_image\\">",
+        "<meta name=\\"twitter:image:src\\" content=\\"http://localhost/__og_image__/og.png\\">",
+        "<meta name=\\"twitter:image:width\\" content=\\"1200\\">",
+        "<meta name=\\"twitter:image:height\\" content=\\"630\\">",
+        "<meta name=\\"twitter:card\\" content=\\"summary_large_image\\">",
+        "<meta name=\\"robots\\" content=\\"noindex, nofollow\\">",
+        "<meta property=\\"og:url\\" content=\\"http://localhost/\\">",
+        "<meta property=\\"og:locale\\" content=\\"en\\">",
+        "<meta property=\\"og:site_name\\" content=\\".playground\\">",
+        "<meta property=\\"og:title\\" content=\\"hello · .playground\\">",
+      ]
+    `)
   }, 60000)
 })
