13ddf30d754a084fac0f0a405932f4115ede7b21
Sergei S. <sergenux@gmail.com>
2024-02-14 18:38:28 +0300

    fix: router hash mode

--- src/runtime/nuxt/middleware/redirect.ts
+++ src/runtime/nuxt/middleware/redirect.ts
@@ -1,27 +1,40 @@
-import { resolveURL, withTrailingSlash, withoutTrailingSlash } from 'ufo'
-import { defineNuxtRouteMiddleware, navigateTo, useError, useRequestURL, useSiteConfig } from '#imports'
+import { resolveURL, withTrailingSlash, withoutTrailingSlash } from "ufo";
+import {
+  defineNuxtRouteMiddleware,
+  navigateTo,
+  useRouter,
+  useError,
+  useRequestURL,
+  useSiteConfig,
+} from "#imports";
 
 export default defineNuxtRouteMiddleware(() => {
-  if (import.meta.prerender)
-    return
+  if (import.meta.prerender) return;
 
-  const siteConfig = useSiteConfig()
-  const error = useError()
-  const url = useRequestURL()
+  const siteConfig = useSiteConfig();
+  const router = useRouter();
+  const error = useError();
+  const url = useRequestURL();
 
-  const originalUrl = url.href
+  const originalUrl = url.href;
 
-  let canonicalUrl
-    = siteConfig.url && !import.meta.dev
-      ? resolveURL(siteConfig.url, url.pathname, url.search, url.hash)
-      : originalUrl
+  let canonicalUrl =
+    siteConfig.url && !import.meta.dev
+      ? resolveURL(
+          withTrailingSlash(siteConfig.url),
+          url.pathname,
+          url.search,
+          url.hash
+        )
+      : originalUrl;
 
-  if (siteConfig.trailingSlash !== undefined) {
+  // @ts-ignore
+  if (siteConfig.trailingSlash !== undefined && !router.options.hashMode) {
     canonicalUrl = siteConfig.trailingSlash
       ? withTrailingSlash(canonicalUrl, true)
-      : withoutTrailingSlash(canonicalUrl, true)
+      : withoutTrailingSlash(canonicalUrl, true);
   }
 
   if (canonicalUrl !== originalUrl && !error.value)
-    return navigateTo(canonicalUrl, { redirectCode: 301, external: true })
-})
+    return navigateTo(canonicalUrl, { redirectCode: 301, external: true });
+});
