4c617d65daa890ba3b2df5d5f4ea90193ce17a45
harlan <harlan@harlanzw.com>
2024-09-02 16:50:29 +1000

    chore: avoid translations
    Closes #295

--- .playground/pages/index.vue
+++ .playground/pages/index.vue
@@ -6,12 +6,5 @@
     <h2 class="text-xl my-10 font-semibold">
       Home Page
     </h2>
-    <p class="text-lg">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-      voluptatibus, quibusdam, quia, quae voluptatem voluptatum quos
-      voluptates quod doloribus quas doloremque. Quisquam voluptatibus,
-      quibusdam, quia, quae voluptatem voluptatum quos voluptates quod
-      doloribus quas doloremque.
-    </p>
   </div>
 </template>
