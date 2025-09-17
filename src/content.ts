9021496abafe20394f7cf626af04698bc48ba6a5
Harlan Wilton <harlan@harlanzw.com>
2025-09-17 15:13:38 +1000

    fix: Nuxt Content v3.7 compatibility

--- src/content.ts
+++ src/content.ts
@@ -1,11 +1,10 @@
 import type { Collection } from '@nuxt/content'
-import type { ZodRawShape } from 'zod'
 import { asRobotsCollection } from '@nuxtjs/robots/content'
 import { asSitemapCollection } from '@nuxtjs/sitemap/content'
 import { asOgImageCollection } from 'nuxt-og-image/content'
 import { asSchemaOrgCollection } from 'nuxt-schema-org/content'
 
-export function asSeoCollection<T extends ZodRawShape>(collection: Collection<T>): Collection<T> {
+export function asSeoCollection<T>(collection: Collection<T>): Collection<T> {
   // run collection through fns
   const fns = [asOgImageCollection, asSchemaOrgCollection, asRobotsCollection, asSitemapCollection]
   return fns.reduce((collection, fn) => fn(collection), collection)
