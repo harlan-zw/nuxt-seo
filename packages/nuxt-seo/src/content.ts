40d4f4481f08628c940a10b8d33d453da62729f3
Harlan Wilton <harlan@harlanzw.com>
2026-03-23 01:54:37 +1100

    fix: deprecate `asSeoCollection`

--- packages/nuxt-seo/src/content.ts
+++ packages/nuxt-seo/src/content.ts
@@ -4,8 +4,12 @@ import { asSitemapCollection } from '@nuxtjs/sitemap/content'
 import { asOgImageCollection } from 'nuxt-og-image/content'
 import { asSchemaOrgCollection } from 'nuxt-schema-org/content'
 
+/**
+ * @deprecated Use `defineRobotsSchema()`, `defineSitemapSchema()`, `defineOgImageSchema()`, and `defineSchemaOrgSchema()` from each module instead.
+ * See https://nuxtseo.com/docs/nuxt-seo/guides/nuxt-content
+ */
 export function asSeoCollection<T>(collection: Collection<T>): Collection<T> {
-  // run collection through fns
+  console.warn('[nuxt-seo] `asSeoCollection()` is deprecated. Import `defineXxxSchema()` from each module and compose them in your collection schema instead. See https://nuxtseo.com/docs/nuxt-seo/guides/nuxt-content')
   const fns = [asOgImageCollection, asSchemaOrgCollection, asRobotsCollection, asSitemapCollection]
   return fns.reduce((collection, fn) => fn(collection), collection)
 }
