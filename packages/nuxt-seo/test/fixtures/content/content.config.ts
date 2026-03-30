2d96bcd98227f39b21482341ecdbd240cec54696
Harlan Wilton <harlan@harlanzw.com>
2026-03-30 21:08:27 +1100

    chore: bump

--- packages/nuxt-seo/test/fixtures/content/content.config.ts
+++ packages/nuxt-seo/test/fixtures/content/content.config.ts
@@ -1,16 +1,19 @@
 import { defineCollection, defineContentConfig, z } from '@nuxt/content'
-import { asSeoCollection } from '../../../src/content'
+import { defineRobotsSchema } from '@nuxtjs/robots/content'
+import { defineSitemapSchema } from '@nuxtjs/sitemap/content'
+import { defineSchemaOrgSchema } from 'nuxt-schema-org/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection(
-      asSeoCollection({
-        type: 'page',
-        source: '**/*.md',
-        schema: z.object({
-          date: z.string().optional(),
-        }),
+    content: defineCollection({
+      type: 'page',
+      source: '**/*.md',
+      schema: z.object({
+        date: z.string().optional(),
+        sitemap: defineSitemapSchema({ z }),
+        robots: defineRobotsSchema({ z }),
+        schemaOrg: defineSchemaOrgSchema({ z }),
       }),
-    ),
+    }),
   },
 })
